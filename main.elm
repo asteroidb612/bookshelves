1ff64263e17fa580f43f7bfc07cfbf8cd08cf324
Drew Lazzeri <drew@interviewing.io>
2018-04-30 22:52:44 -0700

    Visible, configurable book!

--- main.elm
+++ main.elm
@@ -1,17 +1,8 @@
 module Main exposing (main)
 
-import Svg exposing (svg, rect)
-import Svg.Attributes exposing (..)
-import List exposing (range, map)
+import String exposing (left)
+import Hacks exposing (aBook, bookTitles)
+
 
 main =
-    svg [ width <| toString 1000, height <| toString 1000] someRectangles
-
-
-someRectangles =
-    let
-        rectangle iterate =
-            rect [ x (toString (iterate * 50)), width (toString 50), height "100px" ] []
-    in
-        range 0 5
-            |> map rectangle
+    aBook <| left 15 "A man, a Plan, a Canal, Panama"
